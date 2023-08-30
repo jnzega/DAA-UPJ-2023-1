d32c2471a1b25b422c71ff046df79a4380848256
Joshua Nathanael Zega <97107738+jnzega@users.noreply.github.com>
2023-08-31 00:12:19 +0700

    Add files via upload

---
+++ Pertemuan 1/Ubah Variabel/ubahVariabel_PythonCode.py
@@ -0,0 +1,12 @@
+def ubahValue(value1, value2):
+    piring_1 = value1
+    piring_2 = value2
+    piring_3 = 0
+
+    piring_3 = piring_1
+    piring_1 = piring_2
+    piring_2 = piring_3
+
+    print(piring_1 + " " + piring_2)
+
+ubahValue("manggis", "pisang")
