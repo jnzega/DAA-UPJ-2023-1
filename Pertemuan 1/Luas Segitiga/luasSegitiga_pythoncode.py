3223c5ba0aac117e9b7b25c4ab73898f3bdc0c44
Joshua Nathanael Zega <97107738+jnzega@users.noreply.github.com>
2023-08-31 23:09:15 +0700

    Add files via upload

---
+++ Pertemuan 1/Luas Segitiga/luasSegitiga_pythoncode.py
@@ -0,0 +1,12 @@
+def luasSegitiga(valueAlas, valueTinggi):
+    x = valueAlas
+    y = valueTinggi
+
+    hasilLuas = 1/2 * x * y
+
+    return hasilLuas
+
+print(luasSegitiga(25, 30))
+
+#If you running the program above, it will show you the result of the calculation
+#In these case, the program will show 375.0
