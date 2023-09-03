8580b1515c919f94ad16581bc5e4316257c5072d
Joshua Nathanael Zega <97107738+jnzega@users.noreply.github.com>
2023-09-03 13:31:06 +0700

    Add files via upload

---
+++ Pertemuan 1/Luas Jajar Genjang/luasJajarGenjang_pythoncode.py
@@ -0,0 +1,13 @@
+def luasJajarGenjang(valueAlas, valueTinggi):
+
+    x = valueAlas
+    y = valueTinggi
+    
+    hasilLuas = valueAlas*valueTinggi
+
+    return hasilLuas
+
+print(luasJajarGenjang(5, 3))
+
+#If you running the program above, it will show you the result of the calculation
+#In these case, the program will show 15
